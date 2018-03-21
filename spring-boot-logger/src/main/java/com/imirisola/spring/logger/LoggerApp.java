a3fa2fc208f775a686e7bf9a0f340c2340ccef90
I807056 <ivan.mirisola@sap.com>
2018-03-21 14:41:15 -0300

    Initial load

---
+++ spring-boot-logger/src/main/java/com/imirisola/spring/logger/LoggerApp.java
@@ -0,0 +1,13 @@
+package com.imirisola.spring.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerApp.class, args);
+	}
+
+}
