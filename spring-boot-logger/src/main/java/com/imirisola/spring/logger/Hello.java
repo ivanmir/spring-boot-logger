7f933346e8a47cf23e609340fd7ab026a7d351f7
I807056 <ivan.mirisola@sap.com>
2018-03-26 11:15:38 -0300

    +Add all severities for log messages +Add Request Filter to implement MDC - adding static info into the logs

--- spring-boot-logger/src/main/java/com/imirisola/spring/logger/Hello.java
+++ spring-boot-logger/src/main/java/com/imirisola/spring/logger/Hello.java
@@ -1,18 +1,20 @@
 package com.imirisola.spring.logger;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("hello")
 @Slf4j
 public class Hello {
 	
     @GetMapping
     public String getAllDeals() {
+    	log.info("Hello me... meet the real me! ");
+    	log.error("Hello me... meet the real me! ");
+    	log.warn("Hello me... meet the real me! ");
+    	log.debug("Hello me... meet the real me! ");
     	log.trace("Hello me... meet the real me! ");
         return "Hello";
     }
